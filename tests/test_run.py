4943001f405166925b4a85d2a6581f967094cca8
Joost de Folter <folterj@gmail.com>
2026-05-04 16:55:16 +0200

    Improved pipeline testing, minor bugfixes

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,21 +1,24 @@
 import os.path
+import pytest
 import yaml
 
 from src.muvis_align.Pipeline import Pipeline
 
 
-def test(resource_files):
-    for resource_file in resource_files:
-        with open(os.path.join('resources', resource_file), 'r', encoding='utf8') as file:
-            params = yaml.safe_load(file)
+@pytest.mark.parametrize(
+    'resource_file',
+    [
+        'params_test_2d.yml',
+        'params_test_2d_overlay.yml',
+    ],
+)
+def test(resource_file):
+    with open(os.path.join('resources', resource_file), 'r', encoding='utf8') as file:
+        params = yaml.safe_load(file)
 
-        pipeline = Pipeline(params)
-        pipeline.run()
+    pipeline = Pipeline(params)
+    pipeline.run()
 
 
 if __name__ == "__main__":
-    resource_files = [
-        'params_test_2d.yml',
-        #'params_test_2d_overlay.yml',
-    ]
-    test(resource_files)
+    test('params_test_2d.yml')
