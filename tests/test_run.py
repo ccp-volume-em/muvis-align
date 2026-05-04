c1651babbdf3e3f7de6eb8b6c258bcf0d3c0a186
Joost de Folter <folterj@gmail.com>
2026-05-04 17:38:05 +0200

    Update test_run.py

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,24 +1,21 @@
 import os.path
-import pytest
 import yaml
 
 from src.muvis_align.Pipeline import Pipeline
 
 
-@pytest.mark.parametrize(
-    'resource_file',
-    [
+def test():
+    resource_files = [
         'params_test_2d.yml',
         'params_test_2d_overlay.yml',
-    ],
-)
-def test(resource_file):
-    with open(os.path.join('resources', resource_file), 'r', encoding='utf8') as file:
-        params = yaml.safe_load(file)
+    ]
+    for resource_file in resource_files:
+        with open(os.path.join('resources', resource_file), 'r', encoding='utf8') as file:
+            params = yaml.safe_load(file)
 
-    pipeline = Pipeline(params)
-    pipeline.run()
+        pipeline = Pipeline(params)
+        pipeline.run()
 
 
 if __name__ == "__main__":
-    test('params_test_2d.yml')
+    test()
