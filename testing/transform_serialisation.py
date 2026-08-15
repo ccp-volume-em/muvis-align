3509856d0fe871ab7af9d26587b86ac8b51cfc97
Joost de Folter <folterj@gmail.com>
2026-08-15 09:25:08 +0200

    RFC5 transform testing

---
+++ testing/transform_serialisation.py
@@ -0,0 +1,47 @@
+from ngff_zarr.v06.zarr_metadata import Metadata, Dataset, CoordinateSystem,CoordinateSystemIdentifier, Axis, Affine, Scale
+
+
+def metadata_serialisation():
+    metadata = Metadata(
+        datasets=[Dataset(path="example",
+                          coordinateTransformations=[Scale(scale=[1.0, 1.0, 1.0],
+                                                           input=CoordinateSystemIdentifier(name='physical'),
+                                                           output=CoordinateSystemIdentifier(name='source_metadata'),
+                                                           )])
+                  ],
+        coordinateSystems=[
+            CoordinateSystem(
+                name="source_metadata",
+                axes=[
+                    Axis(name="z", type="space"),
+                    Axis(name="y", type="space"),
+                    Axis(name="x", type="space"),
+                ]
+            ),
+            CoordinateSystem(
+                name="registered",
+                axes=[
+                    Axis(name="z", type="space"),
+                    Axis(name="y", type="space"),
+                    Axis(name="x", type="space"),
+                ]
+            )
+        ],
+        coordinateTransformations=[
+            Affine(
+                affine=[
+                    [1.0, 0.0, 0.0, 5.0],
+                    [0.0, 1.0, 0.0, 10.0],
+                    [0.0, 0.0, 1.0, 15.0],
+                    [0.0, 0.0, 0.0, 1.0],
+                ],
+                input=CoordinateSystemIdentifier(name='source_metadata'),
+                output=CoordinateSystemIdentifier(name='registered'),
+            )
+        ],
+    )
+    pass
+
+
+if __name__ == "__main__":
+    metadata_serialisation()
