181f149518c3f6655470ee75ae803e847d34ba21
Joost de Folter <folterj@gmail.com>
2026-08-15 10:32:50 +0200

    Experimenting with ome_zarr_models

--- testing/transform_serialisation.py
+++ testing/transform_serialisation.py
@@ -1,11 +1,32 @@
-# https://ngff-zarr.readthedocs.io/en/latest/rfc5.html
 # https://github.com/bogovicj/ngff-rfc5-coordinate-transformation-examples/
+# https://ome-zarr-models-py.readthedocs.io/en/stable/how-to/
+# https://ome-zarr-models-py.readthedocs.io/en/stable/api/dev/image/#coordinate-transformation-metadata
+# https://github.com/jo-mueller/ome-zarr-models-py/blob/main/tests/v06/test_transforms.py
+# https://github.com/ome-zarr-models/ome-zarr-models-py/blob/main/src/ome_zarr_models/v06/scene.py
+# https://ngff-zarr.readthedocs.io/en/latest/rfc5.html
 
 
-from ngff_zarr.v06.zarr_metadata import Metadata, Dataset, CoordinateSystem,CoordinateSystemIdentifier, Axis, Affine, Scale
+def metadata_models():
+    from ome_zarr_models.v06.coordinate_transforms import (
+        Axis,
+        CoordinateSystem,
+        CoordinateSystemIdentifier,
+        Identity,
+        Scale,
+        Transform,
+    )
+    from ome_zarr_models.v06.multiscales import Dataset
+
+    ct = Identity(
+        input=CoordinateSystemIdentifier(name="a"),
+        output=CoordinateSystemIdentifier(name="b"),
+    )
+    return ct
 
 
-def metadata_serialisation():
+def metadata_nz():
+    from ngff_zarr.v06.zarr_metadata import Metadata, Dataset, CoordinateSystem, CoordinateSystemIdentifier, Axis, Affine, Scale
+
     metadata = Metadata(
         datasets=[Dataset(path="example",
                           coordinateTransformations=[Scale(scale=[1.0, 1.0, 1.0],
@@ -44,7 +65,11 @@ def metadata_serialisation():
             )
         ],
     )
-    pass
+    return metadata
+
+
+def metadata_serialisation():
+    return metadata_nz()
 
 
 if __name__ == "__main__":
