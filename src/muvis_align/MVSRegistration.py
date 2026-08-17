Fix for default filenames, fix transform error in get overlaps, avoid double shape calculation, fixed tests

src/muvis_align/MVSRegistration.py
@@ -57,6 +57,9 @@ class MVSRegistration:
 
     def reset(self):
         self.state = RegState.UNINIT
+        self.source_transform_key = 'source_metadata'
+        self.reg_transform_key = 'registered'
+        self.transition_transform_key = 'transition'
         self.source_metadata = {}
         self.extra_metadata = {}
         self.sims = []
@@ -197,7 +200,7 @@ class MVSRegistration:
             logging.warning('Skipping (no images)')
             return False
 
-        output_filename = operation.split()[0] + 'ed'
+        output_filename = operation_to_past_participle(operation)
 
         self.check_progress(output_filename, output_format)
 
@@ -578,8 +581,7 @@ class MVSRegistration:
                 indexed_key = self.filenames.index(key1), self.filenames.index(key2)
                 indexed_pair_transforms[indexed_key] = (
                     param_utils.affine_to_xaffine(np.array(value['mapping'])).expand_dims({'t': [0]}))
-                if default_quality_key in value:
-                    indexed_qualities[indexed_key] = np.array(value[default_quality_key])
+                indexed_qualities[indexed_key] = np.array(value.get(default_quality_key, 0))
                 if 'bbox' in value:
                     indexed_bboxes[indexed_key] = xr.DataArray(value['bbox'])
             if not is_3d:
@@ -632,7 +634,7 @@ class MVSRegistration:
             metrics = import_json(metrics_filename)
             indexed_metrics = {}
             for key, value in metrics.items():
-                key1, key2 = key.split('-')
+                key1, key2 = json.loads(key)
                 indexed_key = self.filenames.index(key1), self.filenames.index(key2)
                 indexed_metrics[indexed_key] = value
             self.metrics = {
@@ -1462,7 +1464,7 @@ class MVSRegistration:
 
         video.close()
 
-    def get_metrics(self, metric=None, pair=None):
+    def get_metrics(self, metric_key=None, pair=None):
         metrics = self.metrics
         if pair is not None:
             if isinstance(pair, np.ndarray):
@@ -1472,12 +1474,18 @@ class MVSRegistration:
         else:
             if 'summary' in metrics:
                 metrics = metrics['summary']
-        transform_keys = metrics.keys()
-        if len(transform_keys) > 0:
-            transform_key = list(transform_keys)[-1]
-            metrics = metrics.get(transform_key, {})
-        if metric is not None:
-            return metrics.get(metric)
+        reg_transform_key = self.reg_transform_key
+        if reg_transform_key in metrics:
+            transform_key = reg_transform_key
+        elif default_transform_key in metrics:
+            transform_key = default_transform_key
+        elif metrics:
+            transform_key = list(metrics)[-1]
+        else:
+            transform_key = None
+        metrics = metrics.get(transform_key, {})
+        if metric_key is not None:
+            return metrics.get(metric_key)
         else:
             return metrics
 

src/muvis_align/image/util.py
@@ -1176,8 +1176,8 @@ def create_overlap_shapes(sims, transform_key, pairs=None, force_2d=False):
     if pairs is None:
         pairs = np.transpose(np.triu_indices(len(sims), 1))
     for pair in pairs:
-        #sim1, sim2 = squeeze_sim_dims(sims[pair[0]], transform_key), squeeze_sim_dims(sims[pair[1]], transform_key)    # very costly
-        sim1, sim2 = sims[pair[0]], sims[pair[1]]
+        sim1 = squeeze_sim_transform_time(sims[pair[0]], transform_key)
+        sim2 = squeeze_sim_transform_time(sims[pair[1]], transform_key)
         # catch in case there is no overlap
         try:
             result = _get_overlap_bboxes(
@@ -1196,7 +1196,8 @@ def create_overlap_shapes(sims, transform_key, pairs=None, force_2d=False):
                 shape = [[z_position] + list(element) for element in shape]
             shapes.append(shape)
             good_pairs.append(pair)
-        except:
+        except TypeError:
+            # ignore NoneType error if there is no overlap
             pass
     return shapes, good_pairs
 
@@ -1275,6 +1276,7 @@ def combine_transforms(transforms):
 
 
 def squeeze_sim_dims(sim, transform_key):
+    # very costly
     sim = sim.copy()
     if 't' in sim.dims:
         sim = sim.isel(t=0)
@@ -1286,6 +1288,14 @@ def squeeze_sim_dims(sim, transform_key):
     if 'c' in affine.dims:
         affine = affine.isel(c=0)
     si_utils.set_sim_affine(sim, affine, transform_key)
+    return sim
+
+
+def squeeze_sim_transform_time(sim, transform_key):
+    affine = si_utils.get_affine_from_sim(sim, transform_key)
+    if 't' in affine.dims:
+        affine = affine.isel(t=0)
+        si_utils.set_sim_affine(sim, affine, transform_key)
     return sim
 
 

src/muvis_align/ui/Interface.py
@@ -21,9 +21,9 @@ from muvis_align.image.util import get_sim_physical_size, get_sim_position_final
 from muvis_align.file.resources import get_project_template
 from muvis_align.metrics import calc_sims_metrics
 from muvis_align.ui._utils import TemporarilyDisabledWidgets, VisibleActivityDock
-from muvis_align.ui.bilayers_util import get_section_dict, to_magicgui_choices
+from muvis_align.ui.bilayers_util import get_section_dict
 from muvis_align.util import print_dict_simple, set_dict_value, is_valid_value, metric_to_rgb, \
-    calculate_rigid_difference
+    calculate_rigid_difference, operation_to_past_participle
 
 
 class ViewMode(Enum):
@@ -184,7 +184,7 @@ class Interface:
             self.update_metadata_source()
 
     def init_progress(self):
-        output_filename = self.params['registration']['operation'].split()[0] + 'ed'
+        output_filename = operation_to_past_participle(self.params['registration']['operation'])
         self.reg.init_progress(output_filename, zarr_extension)
         if self.reg.is_fused():
             self.enable_tabs(True, 4)
@@ -333,8 +333,8 @@ class Interface:
         if self.params['input_output']['preview_shapes']:
             self._update_view_add_shapes(self.viewer, shapes, refs, labels, face_colors, f'{self.reg.fileset_label} shapes')
 
-        if not force_2d:
-            # Previous shapes need to be recalculated with force_2d=True
+        if is_3d:
+            # Previous 3d shapes need to be recalculated with force_2d=True
             shapes, refs, labels, face_colors = self._calculate_napari_shapes(transform_key, force_2d=True)
         self._clear_napari_view(self.overview)
         self._update_view_add_shapes(self.overview, shapes, refs, labels, face_colors, f'{self.reg.fileset_label} shapes')
@@ -355,7 +355,7 @@ class Interface:
         shapes.extend(shapes2)
         refs += [f'{index1} {index2}' for index1, index2 in pairs]
         labels += ['' for _ in pairs]
-        face_colors += [np.array(metric_to_rgb(self.reg.get_metrics('quality', pair))) for pair in pairs]
+        face_colors += [np.array(metric_to_rgb(self.reg.get_metrics(default_quality_key, pair))) for pair in pairs]
         return shapes, refs, labels, face_colors
 
     def _calculate_napari_data(self, transform_key, fusion_method='additive', show_preprocessed=False):
@@ -425,11 +425,9 @@ class Interface:
                          scale=scale, translate=translate)
 
     def _update_napari_features(self, viewer, fixed_data2, fixed_points, moving_data2, moving_points, matches, inliers):
-
         layers = draw_keypoints_matches_napari(fixed_data2, fixed_points,
                                                moving_data2, moving_points,
                                                matches, inliers, points_color='blue')
-
         viewer.layers.clear()
         for data, kwargs, layer_type in layers:
             if layer_type == "image":
@@ -724,7 +722,7 @@ class Interface:
                                      QMessageBox.Yes | QMessageBox.No)
         if reply == QMessageBox.Yes:
             operation = self.params['registration']['operation']
-            output_filename = operation.split()[-1] + 'ed'
+            output_filename = operation_to_past_participle(operation)
             tile_size = self.params['fusion']['tile_size']
             if ',' in tile_size:
                 tile_size = [int(size.strip()) for size in tile_size.split(',')]