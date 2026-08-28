91d80913172f15e5ba05a0a49265e9ccd8a4c331
Joost de Folter <folterj@gmail.com>
2026-08-28 14:00:26 +0200

    Create xpra-slurm-simple.sh

---
+++ xpra-slurm-simple.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=muvis_align
+#SBATCH --part=ncpu
+#SBATCH --cpus-per-task=64
+#SBATCH --time=1-00:00          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH --mem=640G   # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --output=muvis-align-%j.log
+#SBATCH --error=muvis-align-%j.log
+
+export PYTHONUNBUFFERED=TRUE
+
+# ===========================================================================
+#  EDIT THESE FOUR LINES
+# ===========================================================================
+SIF_PATH="/nemo/stp/ddt/working/defoltj/muvis-align/muvis-align-xpra_v0.3.0.sif"          # container on shared storage
+DATA_DIR="/nemo/project/proj-ccp-vem/datasets"              # directory to expose
+XPRA_PORT=9876                            # port on the compute node
+
+ml Apptainer
+
+apptainer exec "${SIF_PATH}" --bind "${DATA_DIR}:${DATA_DIR}" --bind-tcp="0.0.0.0:${XPRA_PORT}"
